56b0594a6ccbf8614a8c79b712353fad3808e47d
rohan270 <89976867+rohan270@users.noreply.github.com>
2025-12-21 00:33:24 +0530

    Add files via upload

---
+++ num_pattern_gen.py
@@ -0,0 +1,7 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+    return " ".join(str(i) for i in range(1, n + 1))
+print(number_pattern(7))
